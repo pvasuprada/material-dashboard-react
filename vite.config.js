94e46d329b9dd990b1683ba6133be9e0dcdc2d3c
pvasuprada <pvasuprada@gmail.com>
2025-02-11 02:09:32 +0530

    test working build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+      fastRefresh: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,8 +21,33 @@ export default defineConfig({
       routes: path.resolve(__dirname, "./src/routes"),
       store: path.resolve(__dirname, "./src/store"),
       services: path.resolve(__dirname, "./src/services"),
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+      "react-redux": path.resolve(__dirname, "./node_modules/react-redux"),
+      "react-is": path.resolve(__dirname, "./node_modules/react-is"),
+      "prop-types": path.resolve(__dirname, "./node_modules/prop-types"),
+      stream: "stream-browserify",
+      util: "util/",
+      process: "process/browser",
+      events: "events/",
+      buffer: "buffer/",
+      http: "stream-http",
+      https: "https-browserify",
+      url: "url/",
+      assert: "assert/",
+      os: "os-browserify/browser",
+      path: "path-browserify",
+      crypto: "crypto-browserify",
+      zlib: "browserify-zlib",
+      tty: "tty-browserify",
+      fs: "memfs",
     },
     extensions: [".js", ".jsx", ".json"],
+    mainFields: ["browser", "module", "jsnext:main", "jsnext", "main"],
+  },
+  define: {
+    "process.env": {},
+    global: "globalThis",
   },
   server: {
     port: 3000,
@@ -28,12 +58,37 @@ export default defineConfig({
     outDir: "build",
     sourcemap: true,
     commonjsOptions: {
-      include: [],
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+      requireReturnsDefault: "auto",
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "react-redux",
+            "react-is",
+            "prop-types",
+            "@mui/material",
+            "@mui/icons-material",
+            "chart.js",
+            "react-chartjs-2",
+          ],
+        },
+      },
     },
   },
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.[jt]sx?$/,
     exclude: [],
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom", "react-redux", "react-is", "prop-types"],
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
   },
 });
